2793057538400397281e75f7d9e5459f57b62c6f
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-16 13:37:54 -0500

    MarquisTheCoder:  Sat Sep 16 13:37:54 CDT 2023

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -16,7 +16,7 @@
     classes={`window z-20 draggable no-scroll bg-gradient-to-b
     from-black/50 via-black/40 to-black/30 backdrop-blur-lg 
     rounded-md shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] flex flex-col 
-    ${windowClasses}`}
+    ${windowClasses} top`}
     top={top}
     left={left}
     height={height}
