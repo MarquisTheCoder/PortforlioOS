95d85cf85391f991fb2063cc9ed706321da8f5e7
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-17 12:49:37 -0500

    MarquisTheCoder:  Sun Sep 17 12:49:36 CDT 2023

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,9 @@
       <meta property="og:type" content="website">
       <meta name="twitter:card" content="summary_large_image">
       <meta name="twitter:site" content="@YourTwitterHandle">
-      <meta name="twitter:title" content="MarquisTheCoder - Your Source for Web Development Insights"> 
+      <meta name="twitter:title" content="MarquisTheCoder - Your Source for Web Development Insights">
+      <link rel="icon" href="favicon.ico" type="image/x-icon"> 
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  
 </svelte:head>
 
 <slot />
